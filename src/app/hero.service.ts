c68cf7884518c2785eb5244c0e11f89cd008f2aa
Karthik A <karthikthee7@gmail.com>
2020-03-22 09:51:08 +0530

    Hero Detail with Rxjs

---
+++ src/app/hero.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@angular/core";
+import { of, Observable } from "rxjs";
+
+import { Hero } from "./hero";
+import { HEROES } from "./mock-heroes";
+
+@Injectable({
+  providedIn: "root"
+})
+export class HeroService {
+  constructor() {}
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
+}
