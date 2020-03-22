acbc80f007b598e27b40f5b4ae65e271e24571c2
Karthik A <karthikthee7@gmail.com>
2020-03-22 18:26:31 +0530

    Services & In-app Navigation

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,13 +3,21 @@ import { of, Observable } from "rxjs";
 
 import { Hero } from "./hero";
 import { HEROES } from "./mock-heroes";
+import { MessageService } from "./message.service";
 
 @Injectable({
   providedIn: "root"
 })
 export class HeroService {
-  constructor() {}
+  constructor(private messageService: MessageService) {}
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add("HeroService: fetched heroes");
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+
 import { Hero } from "../hero";
 import { HeroService } from "../hero.service";
 
@@ -9,7 +10,7 @@ import { HeroService } from "../hero.service";
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
+
   constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
@@ -17,8 +18,5 @@ export class HeroesComponent implements OnInit {
   }
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
-  }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 }
